46c7e88f52e1d77246566ae9c972d636f1ae7f85
Blasko-Peter <blaskopeter@t_online.hu>
2019-07-30 11:51:41 +0200

    add game.js file to project and I can check the free fields on the table, so the player can click only free button on the table

---
+++ overcomplicated-tictactoe/src/main/resources/static/game.js
@@ -0,0 +1,28 @@
+function checkthebutton (){
+    let buttons = document.getElementsByClassName("checkthebutton");
+    let os = document.getElementsByClassName("fa-circle-o");
+    let xs = document.getElementsByClassName("fa-times");
+    let index = 0;
+    for(let button of buttons){
+        button.addEventListener("click", function () {
+            let buttonId = button.getAttribute("id");
+            for(let o of os){
+                if(o.parentNode.id == buttonId){
+                    index += 1;
+                }
+            }
+            for(let x of xs){
+                if(x.parentNode.id == buttonId){
+                    index += 1;
+                }
+            }
+            if(index == 0){
+                let url = "/game-move?move=" + buttonId;
+                window.location = url;
+            }
+            index = 0;
+        });
+    }
+}
+
+checkthebutton ();
