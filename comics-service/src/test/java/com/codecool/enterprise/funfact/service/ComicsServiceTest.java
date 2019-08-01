913f4476ed57fca5279a3df27d2214f120b6a3e8
Blasko-Peter <blaskopeter@t_online.hu>
2019-08-01 13:06:30 +0200

    add second test to comics-service

--- comics-service/src/test/java/com/codecool/enterprise/funfact/service/ComicsServiceTest.java
+++ comics-service/src/test/java/com/codecool/enterprise/funfact/service/ComicsServiceTest.java
@@ -1,7 +1,10 @@
 package com.codecool.enterprise.funfact.service;
 
+import com.codecool.enterprise.funfact.model.ComicsModel;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -19,6 +22,11 @@ class ComicsServiceTest {
     void generateRandomComicUrl(){
         String[] str = comicsService.generateRandomComicUrl().split("/");
         assertThat(Integer.parseInt(str[3])).isBetween(1,1929);
+    }
+
+    @RepeatedTest(50)
+    void getComic(){
+        assertThat(comicsService.getComic().getImg()).isNotEmpty();
     }
 
 }
